6f199d3b18f975a31fcfe9f904a2cda79783b09f
Tony <tonyel432@gmail.com>
2023-09-13 14:43:53 +0300

    removing credentials

--- ChatDev/Services/EmailSender.cs
+++ ChatDev/Services/EmailSender.cs
@@ -21,17 +21,7 @@ namespace ChatDev.Services
                 client.UseDefaultCredentials = false;
                 client.EnableSsl = true;
                 client.Credentials = new NetworkCredential("chatdevc@gmail.com", "macj ieof mpzh vmwh\r\n");
-                using (var message = new MailMessage(
-                    from: new MailAddress("chatdevc@gmail.com", "ChatDev"),
-                    to: new MailAddress(recipientEmail, recipientName)
-                    ))
-                {
-
-                    message.Subject = "Hello from code!";
-                    message.Body = GetWelcomeEmailHtml(recipientName);
-
-                    client.Send(message);
-                }
+               //PRIVATE
             }
         }
 
